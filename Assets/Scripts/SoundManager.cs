8d535daca9d2904828100fb5096634f59db6fc1d
Mikk Pristavka <mikk@voog.com>
2017-12-02 15:50:40 +0200

    Add test button to SoundManager.PlayMusic

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour {
 	public AudioSource efxSource;
 	public AudioSource musicSource;
-	
+
 	public AudioClip music;
 	public static SoundManager instance = null;
 	void Awake () {
@@ -24,12 +24,32 @@ public class SoundManager : MonoBehaviour {
 		}
 	}
 
-	public void PlaySingle (AudioClip clip) {
+	public void PlaySound (AudioClip clip) {
 		//Set the clip of our efxSource audio source to the clip passed in as a parameter.
 		efxSource.clip = clip;
 
 		//Play the clip.
 		efxSource.Play ();
+	}
+
+	public void PlayMusic (string name) {
+		AudioClip clip = null;
+		if (name == "Music_1") {
+			clip = music;
+		}
+		//Set the clip of our efxSource audio source to the clip passed in as a parameter.
+		musicSource.clip = clip;
+
+		//Play the clip.
+		if (musicSource.clip) {
+			musicSource.Play ();
+		}
+	}
+	
+	public void StopMusic () {
+		musicSource.Stop();
+		
+		musicSource.clip = null;
 	}
 	// Use this for initialization
 	void Start () {
---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour {	
+	public void onButtonPress() {
+		SoundManager.instance.PlayMusic("Music_1");
+	}
+}
