d7e172a8ba5d2f419b41fa82f30b55e952542d8e
Reimo Rebane <reimo.rebane@cyber.ee>
2017-12-02 16:08:46 +0200

    Added some scaffolding code for units and unit groups.

---
+++ Assets/Scripts/UnitGroup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitGroup : MonoBehaviour {
+
+    private List<Unit> groupList = null;
+
+    void Awake () {
+        groupList = new List<Unit>();
+    }
+
+    void Update () {
+        
+    }
+
+    void AddUnit() {
+    }
+
+    void RemoveUnit() {
+    }
+}
