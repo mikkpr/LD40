a1ce552df590f12ebf6ae1717ea3b2e5fa9f6353
Mikk Pristavka <mikk@voog.com>
2017-12-02 14:46:36 +0200

    Add singleton SoundManager and test music track

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+	public AudioSource efxSource;
+	public AudioSource musicSource;
+	public static SoundManager instance = null;
+	void Awake () {
+		{
+			//Check if there is already an instance of SoundManager
+			if (instance == null)
+				//if not, set it to this.
+				instance = this;
+			//If instance already exists:
+			else if (instance != this)
+				//Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
+				Destroy (gameObject);
+
+			//Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+			DontDestroyOnLoad (gameObject);
+		}
+	}
+
+	public void PlaySingle (AudioClip clip) {
+		//Set the clip of our efxSource audio source to the clip passed in as a parameter.
+		efxSource.clip = clip;
+
+		//Play the clip.
+		efxSource.Play ();
+	}
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
