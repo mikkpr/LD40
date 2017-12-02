9519c759a8293c50116443a3451b2d167ee4c0b0
Aare Undo <aare@carto.com>
2017-12-02 16:00:06 +0200

    Fight scene

--- Assets/Scripts/Fight.cs
+++ Assets/Scripts/Fight.cs
@@ -14,7 +14,8 @@ public class Fight : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Space down");    
+            Debug.Log("Space down");
+            SoundManager.instance.PlayMusic("Music_1");
         }
         else if (Input.GetKeyUp(KeyCode.Space))
         {
