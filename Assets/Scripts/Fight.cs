0ed7646ea7a0ac7ecaa66b9555e60e0f2669e535
Aare Undo <aare@carto.com>
2017-12-02 16:43:21 +0200

    basic movement logic

--- Assets/Scripts/Fight.cs
+++ Assets/Scripts/Fight.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 
 public class Fight : MonoBehaviour
 {
+    Vector3 original;
+
     void Start()
     {
         Debug.Log("Start");
+        original = transform.position;
     }
 
     void Update()
@@ -15,11 +18,13 @@ public class Fight : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Debug.Log("Space down");
-            SoundManager.instance.PlayMusic("Music_1");
+            //SoundManager.instance.PlayMusic("Music_1");
+            transform.position = new Vector3(original.x + 1, original.y - 1);
         }
         else if (Input.GetKeyUp(KeyCode.Space))
         {
             Debug.Log("Space up");
+            transform.position = original;
         }
 
     }
