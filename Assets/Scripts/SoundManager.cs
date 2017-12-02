78bdb97d1363be49e0557d323e24e09082e56c69
Aare Undo <aare@carto.com>
2017-12-02 16:08:28 +0200

    cleanup

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -1,63 +1,69 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour {
-	public AudioSource efxSource;
-	public AudioSource musicSource;
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource efxSource;
+    public AudioSource musicSource;
 
-	public AudioClip music;
-	public static SoundManager instance = null;
-	void Awake () {
-		{
-			//Check if there is already an instance of SoundManager
-			if (instance == null)
-				//if not, set it to this.
-				instance = this;
-			//If instance already exists:
-			else if (instance != this)
-				//Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
-				Destroy (gameObject);
+    public AudioClip music;
+    public static SoundManager instance;
 
-			//Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
-			DontDestroyOnLoad (gameObject);
-		}
-	}
+    void Awake()
+    {
+        //Check if there is already an instance of SoundManager
+        if (instance == null)
+            //if not, set it to this.
+            instance = this;
+        //If instance already exists:
+        else if (instance != this)
+            //Destroy this, this enforces our singleton pattern so there can only be one instance of SoundManager.
+            Destroy(gameObject);
 
-	public void PlaySound (AudioClip clip) {
-		//Set the clip of our efxSource audio source to the clip passed in as a parameter.
-		efxSource.clip = clip;
+        //Set SoundManager to DontDestroyOnLoad so that it won't be destroyed when reloading our scene.
+        DontDestroyOnLoad(gameObject);
+    }
 
-		//Play the clip.
-		efxSource.Play ();
-	}
+    public void PlaySound(AudioClip clip)
+    {
+        //Set the clip of our efxSource audio source to the clip passed in as a parameter.
+        efxSource.clip = clip;
 
-	public void PlayMusic (string name) {
-		AudioClip clip = null;
-		if (name == "Music_1") {
-			clip = music;
-		}
-		//Set the clip of our efxSource audio source to the clip passed in as a parameter.
-		musicSource.clip = clip;
+        //Play the clip.
+        efxSource.Play();
+    }
 
-		//Play the clip.
-		if (musicSource.clip) {
-			musicSource.Play ();
-		}
-	}
-	
-	public void StopMusic () {
-		musicSource.Stop();
-		
-		musicSource.clip = null;
-	}
-	// Use this for initialization
-	void Start () {
+    public void PlayMusic(string name)
+    {
+        AudioClip clip = null;
+        if (name == "Music_1")
+        {
+            clip = music;
+        }
+        //Set the clip of our efxSource audio source to the clip passed in as a parameter.
+        musicSource.clip = clip;
 
-	}
+        //Play the clip.
+        if (musicSource.clip)
+        {
+            musicSource.Play();
+        }
+    }
 
-	// Update is called once per frame
-	void Update () {
+    public void StopMusic()
+    {
+        musicSource.Stop();
 
-	}
+        musicSource.clip = null;
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
 }
