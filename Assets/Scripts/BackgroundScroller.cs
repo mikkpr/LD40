9d07d31038773df9aaa30734aa7c9d5a8b68415c
Aare Undo <aare@carto.com>
2017-12-02 18:49:21 +0200

    basic parallax scroller

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileSize;
+
+    Vector3 startPosition;
+
+	void Start()
+	{
+        startPosition = transform.position;
+	}
+
+	void Update()
+	{
+        float position = Mathf.Repeat(Time.time * scrollSpeed, tileSize);
+        transform.position = startPosition + Vector3.left * position;
+	}
+}
