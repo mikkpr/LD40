5653a7bad1f7766544eebc98a4e833db22621b9d
Tiit Pikma <thsnr@users.noreply.github.com>
2017-12-02 19:06:57 +0200

    Rewrite RhythmEngine to manage multiple Units at once.

--- Assets/Scripts/RhythmEngine.cs
+++ Assets/Scripts/RhythmEngine.cs
@@ -1,99 +1,137 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class RhythmEngine : MonoBehaviour
 {
-
-    // Time of the initial beat.
-    public float initial;
-
-    // Interval between beats.
-    public float interval;
+    // Time when the music was started.
+    public float initial = 0.0f;
 
     // Allowed difference from the perfect beat.
-    public float accuracy;
+    public float accuracy = 0.25f;
 
-    // Key that must be pressed to hit beat.
-    public KeyCode key;
+    // KeyCodes that are allocated by the RhythmEngine to Units.
+    public List<KeyCode> allowedKeys = null;
 
-    public Text text;
+    // Units that are following a rhythm.
+    Dictionary<Unit, Tracking> units = null;
 
-    bool inBeat;
-    bool pressed;
+    // KeyCodes that are already allocated to some Units.
+    HashSet<KeyCode> allocatedKeys = null;
 
-    void Start()
+    // Tracking is the rhythm tracking information for a Unit.
+    class Tracking
     {
-        
+        // Has an onBeatStart been issued for this Unit? This is used to avoid
+        // firing an onBeatEnd if no preceding onBeatStart was called.
+        public bool started = false;
+
+        // Is the Unit currently in the rhythm window ("beat"). Units start
+        // with inBeat set to avoid firing an onBeatStart if the Unit was added
+        // in the middle of a beat.
+        public bool inBeat = true;
+
+        // How many times was the KeyCode squence for this Unit pressed in the
+        // current beat.
+        public int pressed = 0;
+
+        // The index of the next Keycode in the Unit's sequence that must be
+        // pressed.
+        public int index = 0;
+    }
+
+    void Awake()
+    {
+        units = new Dictionary<Unit, Tracking>();
+        allocatedKeys = new HashSet<KeyCode>();
+    }
+
+    // AddMarching adds a new marching Unit. It will be allocated a KeyCode
+    // sequence that must be pressed each unit.interval. The KeyCode sequence
+    // will be added to unit.KeyCodes.
+    public void AddMarching(Unit unit)
+    {
+        // Allocate a random unused KeyCode to the unit.
+        // TODO: KeyCode sequences.
+        // XXX: Will loop forever if all keys are allocated.
+        KeyCode key = KeyCode.None; 
+        do
+        {
+            key = allowedKeys[Random.Range(0, allowedKeys.Count)];
+        }
+        while (allocatedKeys.Contains(key));
+        allocatedKeys.Add(key);
+        unit.keyCodes.Add(key);
+
+        // Start tracking the unit.
+        units.Add(unit, new Tracking());
+    }
+
+    public void RemoveMarching(Unit unit)
+    {
+        foreach (KeyCode key in unit.keyCodes)
+        {
+            allocatedKeys.Remove(key);
+        }
+        units.Remove(unit);
+    }
+
+    public void Clear()
+    {
+        units.Clear();
+        allocatedKeys.Clear();
     }
 
     void Update()
     {
-        float sinceInitial = Time.time - initial;
-        float sinceBeat = sinceInitial % interval;
-
-        bool oldInBeat = inBeat;
-        inBeat = sinceBeat < accuracy || sinceBeat > (interval - accuracy);
-
-        if (!oldInBeat && inBeat)
+        foreach (KeyValuePair<Unit, Tracking> entry in units)
         {
-            pressed = false;
-            OnBeatStart();
-        }
-        else if (oldInBeat && !inBeat)
-        {
-            OnBeatEnd();
-            if (!pressed)
+            Unit unit = entry.Key;
+            Tracking tracking = entry.Value;
+
+            // TODO: Use SoundManager.instance.musicSource.time instead.
+            float time = Time.time - initial;
+
+            float sinceBeat = time % unit.interval;
+            bool oldInBeat = tracking.inBeat;
+            tracking.inBeat = sinceBeat < accuracy || sinceBeat > (unit.interval - accuracy);
+
+            if (!oldInBeat && tracking.inBeat)
             {
-                OnBeatMissed();
+                tracking.started = true;
+                tracking.pressed = 0;
+                unit.OnBeatStart();
+            }
+            else if (tracking.started && oldInBeat && !tracking.inBeat)
+            {
+                tracking.index = 0;
+                unit.OnBeatEnd(tracking.pressed == 1);
+            }
+
+            if (tracking.started && Input.GetKeyDown(unit.keyCodes[tracking.index]))
+            {
+                if (!tracking.inBeat)
+                {
+                    unit.OnOutOfBeat();
+                    return;
+                }
+
+                // If this is the last key in the sequence.
+                if (tracking.index == unit.keyCodes.Count - 1)
+                {
+                    if (tracking.pressed > 0)
+                    {
+                        unit.OnBeatDouble();
+                    }
+                    else
+                    {
+                        unit.OnBeatHit();
+                    }
+                    tracking.pressed++;
+                }
+
+                tracking.index = (tracking.index + 1) % unit.keyCodes.Count;
             }
         }
-
-        if (Input.GetKeyDown(key))
-        {
-            if (!inBeat)
-            {
-                OnOutOfBeat();
-                return;
-            }
-            if (pressed)
-            {
-                OnBeatDouble();
-                return;
-            }
-            OnBeatHit();
-            pressed = true;
-        }
-    }
-
-    void OnBeatStart()
-    {
-        text.text = "start";
-    }
-
-    void OnBeatEnd()
-    {
-        text.text = "end";
-    }
-
-    void OnBeatMissed()
-    {
-        text.text = "missed";
-    }
-
-    void OnOutOfBeat()
-    {
-        text.text = "out of beat";
-    }
-
-    void OnBeatDouble()
-    {
-        text.text = "double";
-    }
-
-    void OnBeatHit()
-    {
-        text.text = "hit!";
     }
 }
