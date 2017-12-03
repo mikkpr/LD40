0ba71c68a6b0010be76a200f2931f3d0d7fa663e
Mikk Pristavka <mikk@voog.com>
2017-12-03 23:01:04 +0200

    Stop main menu theme when Fight scene starts

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -56,6 +56,7 @@ public class MainMenu : MonoBehaviour
 
     public void OnStartButtonPress()
     {
+        SoundManager.instance.StopMainMenuTheme();
         SceneManager.LoadScene("Fight");
     }
 
