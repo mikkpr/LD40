9a4e6bd65db5a4394c966dff542096d8a49f9bb1
Aare Undo <aare@carto.com>
2017-12-03 13:33:11 +0200

    MainMenu script cleanup

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,69 +1,83 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour {
-	public GameObject optionsPanel;
-	public Slider volumeSlider;
-	public Text volumeLabel;
-	public Slider musicVolumeSlider;
-	public Text musicVolumeLabel;
+public class MainMenu : MonoBehaviour
+{
+    public GameObject optionsPanel;
+    public Slider volumeSlider;
+    public Text volumeLabel;
+    public Slider musicVolumeSlider;
+    public Text musicVolumeLabel;
 
-	private CanvasGroup optionsPanelCanvasGroup;
+    CanvasGroup optionsPanelCanvasGroup;
 
-	// Use this for initialization
-	void Awake () {
-		optionsPanelCanvasGroup = optionsPanel.GetComponent<CanvasGroup> ();
-		HideOptionsPanel ();
-	}
+    // Use this for initialization
+    void Awake()
+    {
+        optionsPanelCanvasGroup = optionsPanel.GetComponent<CanvasGroup>();
+        HideOptionsPanel();
+    }
 
-	void Start () {
-		volumeSlider.value = SoundManager.instance.GetVolume ();
-		musicVolumeSlider.value = SoundManager.instance.GetMusicVolume ();
-		volumeSlider.onValueChanged.AddListener (delegate { VolumeValueChange (); });
-		musicVolumeSlider.onValueChanged.AddListener (delegate { MusicVolumeValueChange (); });
-	}
+    void Start()
+    {
+        volumeSlider.value = SoundManager.instance.GetVolume();
+        musicVolumeSlider.value = SoundManager.instance.GetMusicVolume();
 
-	void ShowOptionsPanel () {
-		volumeSlider.value = SoundManager.instance.GetVolume ();
-		optionsPanelCanvasGroup.gameObject.SetActive (true);
-	}
+        volumeSlider.onValueChanged.AddListener(VolumeValueChange);
+        musicVolumeSlider.onValueChanged.AddListener(MusicVolumeValueChange);
+    }
 
-	void HideOptionsPanel () {
-		optionsPanelCanvasGroup.gameObject.SetActive (false);
-	}
+    void ShowOptionsPanel()
+    {
+        volumeSlider.value = SoundManager.instance.GetVolume();
+        optionsPanelCanvasGroup.gameObject.SetActive(true);
+    }
 
-	void VolumeValueChange () {
-		volumeLabel.text = volumeSlider.value.ToString () + "%";
-	}
+    void HideOptionsPanel()
+    {
+        optionsPanelCanvasGroup.gameObject.SetActive(false);
+    }
 
-	void MusicVolumeValueChange () {
-		musicVolumeLabel.text = musicVolumeSlider.value.ToString () + "%";
-	}
+    void VolumeValueChange(float volume)
+    {
+        volumeLabel.text = volumeSlider.value.ToString() + "%";
+    }
 
-	public void onStartButtonPress () {
-		Debug.Log ("Pressed Start button");
-	}
+    void MusicVolumeValueChange(float volume)
+    {
+        musicVolumeLabel.text = musicVolumeSlider.value.ToString() + "%";
+    }
 
-	public void onOptionsButtonPress () {
-		Debug.Log ("Pressed Options button");
-		ShowOptionsPanel ();
-	}
+    public void OnStartButtonPress()
+    {
+        Debug.Log("Pressed Start button");
+    }
 
-	public void onQuitButtonPress () {
-		Debug.Log ("Pressed Quit button");
-	}
+    public void OnOptionsButtonPress()
+    {
+        Debug.Log("Pressed Options button");
+        ShowOptionsPanel();
+    }
 
-	public void onOKButtonPress () {
-		Debug.Log ("Pressed OK button");
-		SoundManager.instance.SetVolume (volumeSlider.value);
-		SoundManager.instance.SetMusicVolume (musicVolumeSlider.value);
-		HideOptionsPanel ();
-	}
+    public void OnQuitButtonPress()
+    {
+        Debug.Log("Pressed Quit button");
+    }
 
-	public void onCancelButtonPress () {
-		Debug.Log ("Pressed Cancel button");
-		HideOptionsPanel ();
-	}
+    public void OnOKButtonPress()
+    {
+        Debug.Log("Pressed OK button");
+        SoundManager.instance.SetVolume(volumeSlider.value);
+        SoundManager.instance.SetMusicVolume(musicVolumeSlider.value);
+        HideOptionsPanel();
+    }
+
+    public void OnCancelButtonPress()
+    {
+        Debug.Log("Pressed Cancel button");
+        HideOptionsPanel();
+    }
 }
