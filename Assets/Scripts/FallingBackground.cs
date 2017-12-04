b626f27eee93ce6abbdc2ca9bdd59a709b536a4e
Aare Undo <aare@carto.com>
2017-12-04 11:26:01 +0200

    falling sky background

---
+++ Assets/Scripts/FallingBackground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System;
+
+public class FallingBackground : MonoBehaviour
+{
+    const float minY = 6.0f;
+    const float maxY = -0.8f;
+    const float duration = 3 * 60;
+    const float tick = (minY - maxY) / duration;
+
+    void Start()
+    {
+        transform.position = new Vector3(transform.position.x, minY);
+    }
+
+    void Update()
+    {
+        if (transform.position.y > maxY)
+        {
+            var y = minY - (tick * Time.time);
+            transform.position = new Vector3(transform.position.x, y);
+        }
+    }
+}
